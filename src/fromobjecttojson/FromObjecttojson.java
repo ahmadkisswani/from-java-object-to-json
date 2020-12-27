cb3db98749b3458de53d3aa2c99f34bf7cde1d4f
ahmadkisswani <67906076+ahmadkisswani@users.noreply.github.com>
2020-12-27 12:56:33 +0200

    Update FromObjecttojson.java

--- src/fromobjecttojson/FromObjecttojson.java
+++ src/fromobjecttojson/FromObjecttojson.java
@@ -2,11 +2,25 @@ package fromobjecttojson;
 
 import class_of_users.Users;
 import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.io.FileWriter;
+import java.io.IOException;
+import static java.nio.file.Files.lines;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FromObjecttojson {
+
+    private static FileWriter file;
+    private static ObjectMapper mapper;
+    private static Users us = null;
 
     public static void main(String[] args) throws JsonProcessingException {
         Users us = new Users();
@@ -15,13 +29,25 @@ public class FromObjecttojson {
         us.setGender("m");
         us.setMedName("mohammad");
         us.setLastName("kisswani");
-        ObjectMapper mapper = new ObjectMapper();
+        mapper = new ObjectMapper();
         try {
 
             System.out.println(mapper.writeValueAsString(us));
+            //jsonFile
+            file = new FileWriter(System.getProperty("user.dir") + "/jsonFile/jsonFile.json");
+  
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String prettyJson = gson.toJson(mapper.writeValueAsString(us));
+            file.write(prettyJson);
+
+            file.flush();
+            file.close();
+
         } catch (JsonGenerationException | JsonMappingException e) {
             // catch various errors
             e.printStackTrace();
+        } catch (IOException ex) {
+            Logger.getLogger(FromObjecttojson.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
